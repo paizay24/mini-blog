d7d2c534c121a094f9bda7f65f0527bd7da79392
paizay24 <paizay.dev24@gmail.com>
2024-07-12 20:24:18 +0630

    test

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
-use App\Http\Controllers\PostController;
+
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/',[PostController::class,'index'])->name('post.index');
+// Route::get('/',[PostController::class,'index'])->name('post.index');
 
-Route::get('/post-create', [PostController::class,'create'])->name('post.create');
-Route::post('/post-create', [PostController::class,'store'])->name('post.store');
-Route::get('/post-show/{id}', [PostController::class,'show'])->name('post.show');
-Route::delete('/post-delete/{id}',[PostController::class,'delete'])->name('post.delete');
-Route::get('/post-edit/{id}', [PostController::class,'edit'])->name('post.edit');
-Route::put('/post-update/{id}', [PostController::class,'update'])->name('post.update');
+// Route::get('/post-create', [PostController::class,'create'])->name('post.create');
+// Route::post('/post-create', [PostController::class,'store'])->name('post.store');
+// Route::get('/post-show/{id}', [PostController::class,'show'])->name('post.show');
+// Route::delete('/post-delete/{id}',[PostController::class,'delete'])->name('post.delete');
+// Route::get('/post-edit/{id}', [PostController::class,'edit'])->name('post.edit');
+// Route::put('/post-update/{id}', [PostController::class,'update'])->name('post.update');
 
 
